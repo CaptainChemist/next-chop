e2180001c4b0401f12d51d941dd0d19226355d0c
Stephen Jensen <stephen.jensen3@gmail.com>
2019-12-28 13:57:08 -0600

    3. Add Ant Design

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import 'antd/dist/antd.css';
+import App from 'next/app';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
