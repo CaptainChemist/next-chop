9c9c637abab0d750dba9ccdb526ca05b275c45f6
Stephen Jensen <stephen.jensen3@gmail.com>
2019-12-28 13:46:42 -0600

    2. Add Styled Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.h1`
+  font-size: 2em;
+  text-align: left;
+  padding: 16px 16px;
+`;
+
 const Index = () => {
   return (
     <div>
-      <p>Index Page</p>
+      <StyledHeader>Index Page</StyledHeader>
     </div>
   );
 };
