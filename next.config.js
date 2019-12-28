b72a7bd29150bac341c239a43ae4dc6410f1d22a
Stephen Jensen <stephen.jensen3@gmail.com>
2019-12-28 17:17:40 -0600

    10. Add GraphCMS

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
 const withCSS = require('@zeit/next-css');
+require('dotenv').config();
 
-module.exports = withCSS({});
+const {
+  APIURL,
+  APIKEY,
+  PROJECTID,
+  BRANCH,
+  CDNBASE,
+  GRAPHCMSID,
+  GRAPHCMSURL,
+} = process.env;
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    graphcms: {
+      APIURL,
+      APIKEY,
+      PROJECTID,
+      BRANCH,
+      CDNBASE,
+      GRAPHCMSID,
+      GRAPHCMSURL,
+    },
+  },
+});
